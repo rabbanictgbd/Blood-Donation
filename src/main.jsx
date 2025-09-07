4658a4a12ad6b024097491cefb377a823b0ce1db
User06 <user06@vdesktop.sbcnoc.com>
2025-09-07 18:02:03 +0600

    added layout file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './index.css';
+// import './index.css';
+import './app.css';
 import AuthProvider from "./context/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
